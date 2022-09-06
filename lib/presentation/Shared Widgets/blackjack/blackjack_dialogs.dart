1113d8e307254a4fae92a47685649153ce2ab1c8
millenmarbun24 <millenmarbun24@gmail.com>
2022-09-06 14:39:31 +0700

    xd

--- lib/presentation/Shared Widgets/blackjack/blackjack_dialogs.dart
+++ lib/presentation/Shared Widgets/blackjack/blackjack_dialogs.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutterapibloc1/presentation/Shared%20Widgets/blackjack/AppBar.dart';
 import 'package:flutterapibloc1/presentation/routes/route_const.dart';
 import 'package:flutterapibloc1/presentation/screens/blackjack/blackjackScreens.dart';
 
 class BJDialogs {
+  static var txt = TextEditingController();
+
   static Future<void> showMyWinDialog(context /*, Future<int> set*/) async {
     return showDialog<void>(
       context: context,
@@ -90,6 +93,168 @@ class BJDialogs {
           ],
         );
       },
+    );
+  }
+
+  static Future<void> showBetDialog(context, Function bet) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) => new AlertDialog(
+        backgroundColor: const Color(0xff1C5220),
+        insetPadding: EdgeInsets.zero,
+        content: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.35,
+            child: Column(
+              children: [
+                Center(
+                    child: new Text(
+                  "Place your bet",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      child: Image.asset(
+                        "assets/1.png",
+                        scale: 10,
+                      ),
+                      onTap: () {
+                        txt.text = "1";
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset(
+                        "assets/5.png",
+                        scale: 10,
+                      ),
+                      onTap: () {
+                        txt.text = "5";
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset(
+                        "assets/10.png",
+                        scale: 10,
+                      ),
+                      onTap: () {
+                        txt.text = "10";
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset(
+                        "assets/20.png",
+                        scale: 10,
+                      ),
+                      onTap: () {
+                        txt.text = "20";
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset(
+                        "assets/50.png",
+                        scale: 10,
+                      ),
+                      onTap: () {
+                        txt.text = "50";
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset(
+                        "assets/100.png",
+                        scale: 10,
+                      ),
+                      onTap: () {
+                        txt.text = "100";
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      width: 40,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green,
+                            shape: const StadiumBorder(),
+                            side: BorderSide(
+                              width: 2,
+                              color: Colors.white,
+                            )),
+                        child: Text("½"),
+                        onPressed: () {
+                          txt.text = (int.parse(txt.text) / 2).toString();
+                        },
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2.5,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xFF0A3E31),
+                      ),
+                      child: TextField(
+                        controller: txt,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (val) {
+                          bet(val);
+                        },
+                      ),
+                    ),
+                    Container(
+                      width: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green,
+                            shape: const StadiumBorder(),
+                            side: BorderSide(
+                              width: 2,
+                              color: Colors.white,
+                            )),
+                        child: Text("x2"),
+                        onPressed: () {
+                          txt.text = (int.parse(txt.text) * 2).toString();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Minumum \$10 | Maximum \$1000",
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    bet(int.parse(txt.text));
+                  },
+                  child: Text("Done"),
+                  style:
+                      ElevatedButton.styleFrom(primary: Colors.green.shade400),
+                )
+              ],
+            )),
+      ),
     );
   }
 }
--- lib/presentation/screens/blackjack/blackjackScreens.dart
+++ lib/presentation/screens/blackjack/blackjackScreens.dart
@@ -60,166 +60,15 @@ class _BlackJacScreenState extends State<BlackJacScreen> {
     super.initState();
   }
 
+  void changeBetVal(int betValue) {
+    setState(() {
+      betVal = betValue;
+    });
+  }
+
   void dialogBet() {
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-      await showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => new AlertDialog(
-          backgroundColor: const Color(0xff1C5220),
-          insetPadding: EdgeInsets.zero,
-          content: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.35,
-              child: Column(
-                children: [
-                  Center(
-                      child: new Text(
-                    "Place your bet",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 21,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )),
-                  SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      GestureDetector(
-                        child: Image.asset(
-                          "assets/1.png",
-                          scale: 10,
-                        ),
-                        onTap: () {
-                          txt.text = "1";
-                        },
-                      ),
-                      GestureDetector(
-                        child: Image.asset(
-                          "assets/5.png",
-                          scale: 10,
-                        ),
-                        onTap: () {
-                          txt.text = "5";
-                        },
-                      ),
-                      GestureDetector(
-                        child: Image.asset(
-                          "assets/10.png",
-                          scale: 10,
-                        ),
-                        onTap: () {
-                          txt.text = "10";
-                        },
-                      ),
-                      GestureDetector(
-                        child: Image.asset(
-                          "assets/20.png",
-                          scale: 10,
-                        ),
-                        onTap: () {
-                          txt.text = "20";
-                        },
-                      ),
-                      GestureDetector(
-                        child: Image.asset(
-                          "assets/50.png",
-                          scale: 10,
-                        ),
-                        onTap: () {
-                          txt.text = "50";
-                        },
-                      ),
-                      GestureDetector(
-                        child: Image.asset(
-                          "assets/100.png",
-                          scale: 10,
-                        ),
-                        onTap: () {
-                          txt.text = "100";
-                        },
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        width: 40,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              primary: Colors.green,
-                              shape: const StadiumBorder(),
-                              side: BorderSide(
-                                width: 2,
-                                color: Colors.white,
-                              )),
-                          child: Text("½"),
-                          onPressed: () {
-                            txt.text = (int.parse(txt.text) / 2).toString();
-                          },
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width / 2.5,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Color(0xFF0A3E31),
-                        ),
-                        child: TextField(
-                          controller: txt,
-                          keyboardType: TextInputType.number,
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                          ),
-                          onChanged: (val) {
-                            betVal = int.parse(val);
-                          },
-                        ),
-                      ),
-                      Container(
-                        width: 50,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              primary: Colors.green,
-                              shape: const StadiumBorder(),
-                              side: BorderSide(
-                                width: 2,
-                                color: Colors.white,
-                              )),
-                          child: Text("x2"),
-                          onPressed: () {
-                            txt.text = (int.parse(txt.text) * 2).toString();
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    "Minumum \$10 | Maximum \$1000",
-                    style: TextStyle(color: Colors.white, fontSize: 12),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text("Done"),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.green.shade400),
-                  )
-                ],
-              )),
-        ),
-      );
+      await BJDialogs.showBetDialog(context, changeBetVal);
     });
   }
 
@@ -289,9 +138,13 @@ class _BlackJacScreenState extends State<BlackJacScreen> {
                           },
                         ),
                       ),
-                      SizedBox(height: 1),
-                      ElevatedButton(
-                          onPressed: () => dialogBet(), child: Text("k")),
+                      SizedBox(height: 15),
+                      /*ElevatedButton(
+                          onPressed: () {
+                            dialogBet();
+                            setBal(temp! + 120);
+                          },
+                          child: Text("k")),*/
                       //missing
                       BlocBuilder<BlackjackkBloc, BlackjackkState>(
                           bloc: dealbloc,
@@ -369,7 +222,8 @@ class _BlackJacScreenState extends State<BlackJacScreen> {
                                 valuebloc.add(WinEvent());
                               }
                             }
-                            if (state.dealerVal == playerVal) {
+                            if (state.dealerVal == playerVal &&
+                                state.dealerVal > 16) {
                               valuebloc.add(DrawEvent());
                             }
                             return Text(state.dealerVal.toString(),
@@ -408,6 +262,11 @@ class _BlackJacScreenState extends State<BlackJacScreen> {
                             Future.delayed(Duration(milliseconds: 700), () {
                               BJDialogs.showMyLoseDialog(context, playerBal);
                               setBal(temp! - betVal);
+                            });
+                          }
+                          if (state is DrawState) {
+                            Future.delayed(Duration(milliseconds: 700), () {
+                              BJDialogs.showMyDrawDialog(context);
                             });
                           }
                         },
